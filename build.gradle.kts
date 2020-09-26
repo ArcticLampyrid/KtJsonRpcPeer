8c05df2f9a9dc05f4ddf46218f1bf1db0dcb1d59
qiqiworld <1354092549@qq.com>
2020-09-26 20:55:58 +0800

    release: v0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ configure<PublishingExtension> {
         create<MavenPublication>("maven") {
             groupId = "ktjsonrpcpeer"
             artifactId = "ktjsonrpcpeer"
-            version = "0.2"
+            version = "0.3"
             from(components["java"])
         }
     }
