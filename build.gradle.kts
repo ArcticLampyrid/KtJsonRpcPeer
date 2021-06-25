9e45e87089a5b2269386e9da09ea3cb9ed13540f
ArcticLampyrid <ArcticLampyrid@outlook.com>
2021-06-25 22:58:03 +0800

    release: v0.12.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 apply(plugin = "kotlinx-atomicfu")
 group = "com.github.ArcticLampyrid.KtJsonRpcPeer"
 if (version.toString() == "unspecified") {
-    version = "0.12.1"
+    version = "0.12.2"
 }
 kotlin {
     explicitApi()
